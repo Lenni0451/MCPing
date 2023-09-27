6b97213549a22740ca700d527548491d2292dfd2
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-09-27 22:00:58 +0200

    Fixed javadoc

--- src/main/java/net/lenni0451/mcping/pings/sockets/factories/ITCPSocketFactory.java
+++ src/main/java/net/lenni0451/mcping/pings/sockets/factories/ITCPSocketFactory.java
@@ -3,8 +3,6 @@ package net.lenni0451.mcping.pings.sockets.factories;
 import net.lenni0451.mcping.ServerAddress;
 import net.lenni0451.mcping.pings.sockets.types.ITCPSocket;
 
-import java.io.IOException;
-
 /**
  * A factory interface to create a new {@link ITCPSocket} instance.
  */
@@ -18,7 +16,6 @@ public interface ITCPSocketFactory {
      * @param connectTimeout The connect timeout
      * @param readTimeout    The read timeout
      * @return The connected socket
-     * @throws IOException If an I/O error occurs
      */
     ITCPSocket create(final ServerAddress serverAddress, final int connectTimeout, final int readTimeout);
 
--- src/main/java/net/lenni0451/mcping/pings/sockets/factories/IUDPSocketFactory.java
+++ src/main/java/net/lenni0451/mcping/pings/sockets/factories/IUDPSocketFactory.java
@@ -3,8 +3,6 @@ package net.lenni0451.mcping.pings.sockets.factories;
 import net.lenni0451.mcping.ServerAddress;
 import net.lenni0451.mcping.pings.sockets.types.IUDPSocket;
 
-import java.io.IOException;
-
 /**
  * A factory interface to create a new {@link IUDPSocket} instance.
  */
@@ -17,7 +15,6 @@ public interface IUDPSocketFactory {
      * @param serverAddress The server address to connect to
      * @param readTimeout   The read timeout
      * @return The connected socket
-     * @throws IOException If an I/O error occurs
      */
     IUDPSocket create(final ServerAddress serverAddress, final int readTimeout);
 
